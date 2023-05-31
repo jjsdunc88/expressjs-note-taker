8a0f307ebd5c0b1dccaec8248e60bd8a5fd748b5
JDunc <jjsduncan@gmail.com>
2023-05-31 19:29:39 -0400

    corrected id variable to get functionality fro getNotes function

--- 02-Challenge/Develop/routes/api-routes.js
+++ 02-Challenge/Develop/routes/api-routes.js
@@ -25,7 +25,7 @@ router.post('/notes', (req, res) => {
         const newNote = {
             title,
             text,
-            note_id: uuid(),
+            id: uuid(),
         };
 
         db.push(newNote);
